830e2b70fb86197b30be15cb4b9d75c7c1a23ffa
TubSticks <matthijsvanderwal99@hotmail.com>
2021-12-07 15:38:15 +0100

    Changed: removed commenting in DNS script

--- G05-DNS01/configure-dns.sh
+++ G05-DNS01/configure-dns.sh
@@ -19,31 +19,31 @@ sed -i 's+listen-on port 53 { 127.0.0.1; };+listen-on port 53 { any; };+' /etc/n
 sed -i 's+listen-on-v6 port 53 { ::1; };+// listen-on-v6 port 53 { ::1; };+' /etc/named.conf
 sed -i 's+allow-query     { localhost; };+allow-query     { localhost; any; };+' /etc/named.conf
 
-#echo 'zone "groep5.local" {' >> /etc/named.conf
-#echo '	type master;' >> /etc/named.conf
-#echo '	file "db.groep5.local";' >> /etc/named.conf
-#echo '	allow-transfer { 10.15.1.20; };' >> /etc/named.conf
-#echo '	also-notify { 10.15.1.20; };' >> /etc/named.conf
-#echo '};' >> /etc/named.conf
+echo 'zone "groep5.local" {' >> /etc/named.conf
+echo '	type master;' >> /etc/named.conf
+echo '	file "db.groep5.local";' >> /etc/named.conf
+echo '	allow-transfer { 10.15.1.20; };' >> /etc/named.conf
+echo '	also-notify { 10.15.1.20; };' >> /etc/named.conf
+echo '};' >> /etc/named.conf
 
 # Configure db.groep5.local
-#cat > /var/named/db.groep5.local
-#echo ';' >> /var/named/db.groep5.local
-#echo '; BIND data file for domain' >> /var/named/db.groep5.local
-#echo ';' >> /var/named/db.groep5.local
-#echo '$TTL	604800' >> /var/named/db.groep5.local
-#echo '@	IN	SOA	g05-ns01.groep5.local.	root.localhost. (' >> /var/named/db.groep5.local
-#echo '			      3		;	Serial' >> /var/named/db.groep5.local
-#echo '			 604800		;	Refresh' >> /var/named/db.groep5.local
-#echo '			  86400 	;	Retry' >> /var/named/db.groep5.local
-#echo '			2419200		;	Expire' >> /var/named/db.groep5.local
-#echo '			 604800	)	;	Negative Cache TTL' >> /var/named/db.groep5.local
-#echo ';' >> /var/named/db.groep5.local
-#echo '@	IN	NS	g05-ns01.groep5.local.' >> /var/named/db.groep5.local
-#echo '@	IN	A	10.15.1.14' >> /var/named/db.groep5.local
-#echo 'g05-ns01	IN	A	10.15.1.20' >> /var/named/db.groep5.local
-#echo 'g05-dc01	IN	A	10.15.1.14' >> /var/named/db.groep5.local
-#echo 'moodle	IN	A	10.15.1.12' >> /var/named/db.groep5.local
+cat > /var/named/db.groep5.local
+echo ';' >> /var/named/db.groep5.local
+echo '; BIND data file for domain' >> /var/named/db.groep5.local
+echo ';' >> /var/named/db.groep5.local
+echo '$TTL	604800' >> /var/named/db.groep5.local
+echo '@	IN	SOA	g05-ns01.groep5.local.	root.localhost. (' >> /var/named/db.groep5.local
+echo '			      3		;	Serial' >> /var/named/db.groep5.local
+echo '			 604800		;	Refresh' >> /var/named/db.groep5.local
+echo '			  86400 	;	Retry' >> /var/named/db.groep5.local
+echo '			2419200		;	Expire' >> /var/named/db.groep5.local
+echo '			 604800	)	;	Negative Cache TTL' >> /var/named/db.groep5.local
+echo ';' >> /var/named/db.groep5.local
+echo '@	IN	NS	g05-ns01.groep5.local.' >> /var/named/db.groep5.local
+echo '@	IN	A	10.15.1.14' >> /var/named/db.groep5.local
+echo 'g05-ns01	IN	A	10.15.1.20' >> /var/named/db.groep5.local
+echo 'g05-dc01	IN	A	10.15.1.14' >> /var/named/db.groep5.local
+echo 'moodle	IN	A	10.15.1.12' >> /var/named/db.groep5.local
 
 # Check if everything was configured correctly
 named-checkconf
