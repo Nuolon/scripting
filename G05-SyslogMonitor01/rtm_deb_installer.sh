29f0f39cf1f3d6c711bec082f5d67aa41098eaa7
Joris Rijs <joris@freya22.nl>
2022-01-10 20:24:28 +0100

    Added auditbeat install to the rtm config scripts

--- G05-SyslogMonitor01/rtm_deb_installer.sh
+++ G05-SyslogMonitor01/rtm_deb_installer.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-# Downloading metricbeat and installing the service
-curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-7.16.2-amd64.deb
-sudo dpkg -i metricbeat-7.16.2-amd64.deb
 
-# Configuring central elasticsearch and kibana endpoints
-echo '
+elastic_kibana_config = '
 output.elasticsearch:
   hosts: ["10.15.1.16:9200"]
   username: "elastic"
   password: "changeme"
 setup.kibana:
   host: "10.15.1.16:5601"
-' >> /etc/metricbeat/metricbeat.yml
+'
+
+# Downloading metricbeat and installing the service
+curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-7.16.2-amd64.deb
+sudo dpkg -i metricbeat-7.16.2-amd64.deb
+
+# Configuring central elasticsearch and kibana endpoints
+echo $elastic_kibana_config >> /etc/metricbeat/metricbeat.yml
 
 metricbeat modules enable system
 sudo metricbeat setup
@@ -21,16 +24,17 @@ sudo service metricbeat start
 curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.16.2-amd64.deb
 sudo dpkg -i filebeat-7.16.2-amd64.deb
 
-echo '
-output.elasticsearch:
-  hosts: ["10.15.1.16:9200"]
-  username: "elasticsearch"
-  password: "changeme" 
-
-setup.kibana:
-  host: "10.15.1.16:5601" 
-' >> /etc/filebeat/filebeat.yml
+echo $elastic_kibana_config >> /etc/filebeat/filebeat.yml
 
 filebeat modules enable system
 filebeat setup -e
 service enable filebeat.service
+
+curl -L -O https://artifacts.elastic.co/downloads/beats/auditbeat/auditbeat-7.16.2-amd64.deb
+sudo dpkg -i auditbeat-7.16.2-amd64.deb
+
+echo $elastic_kibana_config >> /etc/auditbeat/auditbeat.yml
+
+auditbeat setup -e
+sudo service auditbeat start 
+sudo systemctl enable auditbeat
--- G05-SyslogMonitor01/rtm_rpm_installer.sh
+++ G05-SyslogMonitor01/rtm_rpm_installer.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-7.15.2-x86_64.rpm
-sudo rpm -vi metricbeat-7.15.2-x86_64.rpm
-echo '
+elastic_kibana_config = '
 output.elasticsearch:
   hosts: ["10.15.1.16:9200"]
   username: "elastic"
   password: "changeme"
 setup.kibana:
   host: "10.15.1.16:5601"
-' >> /etc/metricbeat/metricbeat.yml
+' 
+
+curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-7.15.2-x86_64.rpm
+sudo rpm -vi metricbeat-7.15.2-x86_64.rpm
+echo $elastic_kibana_config >> /etc/metricbeat/metricbeat.yml
 
 metricbeat modules enable system
 sudo metricbeat setup
@@ -19,16 +21,20 @@ sudo service metricbeat start
 curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.16.2-x86_64.rpm
 sudo rpm -vi filebeat-7.16.2-x86_64.rpm
 
-echo '
-output.elasticsearch:
-  hosts: ["10.15.1.16:9200"]
-  username: "elasticsearch"
-  password: "changeme" 
-
-setup.kibana:
-  host: "10.15.1.16:5601" 
-' >> /etc/filebeat/filebeat.yml
+echo $elastic_kibana_config >> /etc/filebeat/filebeat.yml
 
 filebeat modules enable system
 filebeat setup -e
 service enable filebeat.service
+
+# Installing auditbeat
+curl -L -O https://artifacts.elastic.co/downloads/beats/auditbeat/auditbeat-7.16.2-x86_64.rpm
+sudo rpm -vi auditbeat-7.16.2-x86_64.rpm
+
+#sed /$hosts: ["localhost:9200"]/d /etc/auditbeat/auditbeat.yml
+
+echo $elastic_kibana_config >> /etc/auditbeat/auditbeat.yml
+
+auditbeat setup -e
+sudo service auditbeat start 
+sudo systemctl enable auditbeat
